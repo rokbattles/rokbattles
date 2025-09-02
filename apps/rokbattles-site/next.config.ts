e112c3eca7e0eb0556a307fd4e38b8ba8067e53e
nsylke <hello@nsylke.me>
2025-09-02 18:13:00 -0500

    refactor: update next config

--- apps/rokbattles-site/next.config.ts
+++ apps/rokbattles-site/next.config.ts
@@ -3,7 +3,20 @@ import type { NextConfig } from "next";
 const isProvEnv = process.env.NODE_ENV === "production";
 
 const config: NextConfig = {
+  compiler: {
+    reactRemoveProperties: true,
+    removeConsole: isProvEnv,
+  },
+  compress: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
   output: "standalone",
+  poweredByHeader: false,
+  productionBrowserSourceMaps: false,
   reactStrictMode: !isProvEnv,
 };
 
