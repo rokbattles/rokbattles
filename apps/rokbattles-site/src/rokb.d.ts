e121f55f38cdbca03a67659fb01633c7133dde73
nsylke <hello@nsylke.me>
2025-09-07 21:27:43 -0500

    chore: add type augmentation for process env

--- apps/rokbattles-site/src/rokb.d.ts
+++ apps/rokbattles-site/src/rokb.d.ts
@@ -1,5 +1,14 @@
 import { routing } from "@/i18n/routing";
 
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      ROKB_API_URL: string;
+      ROKB_DATASETS_PATH: string;
+    }
+  }
+}
+
 declare module "next-intl" {
   interface AppConfig {
     Locale: (typeof routing.locales)[number];
