b61eb791431f25d02b9ec84644e7190289669150
nsylke <hello@nsylke.me>
2025-10-13 19:20:58 -0500

    chore: bump deps

--- apps/rokbattles-platform/src/app/robots.ts
+++ apps/rokbattles-platform/src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
         userAgent: "*",
       },
     ],
-    sitemap: "https://rokbattles.com/sitemap.xml",
+    sitemap: "https://platform.rokbattles.com/sitemap.xml",
   };
 }
