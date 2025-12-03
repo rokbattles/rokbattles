9233deae7e5bf012be8756a212b3de282b441b59
nsylke <hello@nsylke.me>
2025-12-03 16:01:31 -0600

    refactor: initialize spring boot

---
+++ src/main/java/com/sylketech/rokbattles/RokbattlesApplication.java
@@ -0,0 +1,13 @@
+package com.sylketech.rokbattles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RokbattlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RokbattlesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sylketech/rokbattles/RokbattlesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sylketech.rokbattles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RokbattlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
