d708050d6710ff3686c296b3e4c250f6986e360e
nsylke <hello@nsylke.me>
2025-12-07 12:05:00 -0600

    feat: add react-router and configure index route

--- src/main/js/src/main.tsx
+++ src/main/js/src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App.tsx";
 
 // biome-ignore lint/style/noNonNullAssertion: ignore
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
