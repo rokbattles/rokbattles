e484dcf8de9c1e891dd158f972947ae4683229dc
nsylke <hello@nsylke.me>
2025-12-13 12:00:29 -0600

    refactor: implement slash command handler & help command content

--- apps/rokbattles-bot/src/commands/system/HelpCommand.ts
+++ apps/rokbattles-bot/src/commands/system/HelpCommand.ts
@@ -1,4 +1,4 @@
-import { ApplicationCommandType, MessageFlags, SectionBuilder } from "discord.js";
+import { ApplicationCommandType, ContainerBuilder, MessageFlags } from "discord.js";
 import type { BaseClient } from "@/lib/BaseClient";
 import type { CommandHandler } from "@/lib/CommandHandler";
 
@@ -9,24 +9,21 @@ export const HelpCommand: CommandHandler<BaseClient> = {
     type: ApplicationCommandType.ChatInput,
   },
   async chatInput(_client, interaction, _args) {
-    const helpSection = new SectionBuilder().addTextDisplayComponents(
-      (builder) =>
-        builder.setContent(
-          "ROK Battles is a community-driven platform for sharing, exploring, and analyzing battle reports and viewing trends in Rise of Kingdoms."
-        ),
-      (builder) =>
-        builder.setContent(
-          [
-            "### Relevant Links",
-            "* Platform: <https://platform.rokbattles.com>",
-            "* Desktop app: <https://platform.rokbattles.com/desktop-app>",
-            "* Support: <https://platform.rokbattles.com/support>",
-          ].join("\n")
-        )
+    const helpContainer = new ContainerBuilder().addTextDisplayComponents((builder) =>
+      builder.setContent(
+        [
+          "## ROK Battles",
+          "A community-driven platform for sharing, exploring, and analyzing battle reports and viewing trends in Rise of Kingdoms.",
+          "### Relevant Links",
+          "* Platform: <https://platform.rokbattles.com>",
+          "* Desktop app: <https://platform.rokbattles.com/desktop-app>",
+          "* Support: <https://platform.rokbattles.com/discord>",
+        ].join("\n")
+      )
     );
 
     await interaction.reply({
-      components: [helpSection],
+      components: [helpContainer],
       flags: MessageFlags.IsComponentsV2,
     });
   },
