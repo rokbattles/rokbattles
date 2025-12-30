5ea7facd7c01b999dfac7aa33ebd5be1a32b7671
Nicholas Sylke <hello@nsylke.me>
2025-12-30 11:36:39 -0600

    feat: add cookie settings dialog (#206)

--- apps/rokbattles-platform/src/components/PlatformAccountDropdownMenu.tsx
+++ apps/rokbattles-platform/src/components/PlatformAccountDropdownMenu.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import {
   ArrowRightStartOnRectangleIcon,
   Cog6ToothIcon,
+  ScaleIcon,
   ShieldCheckIcon,
 } from "@heroicons/react/16/solid";
+import { useState } from "react";
+import { CookieConsentDialog } from "@/components/CookieConsentDialog";
 import {
   DropdownDivider,
   DropdownItem,
@@ -17,25 +22,34 @@ export function PlatformAccountDropdownMenu({
   anchor: "top start" | "bottom end";
   handleLogout: () => Promise<void>;
 }) {
+  const [isCookieDialogOpen, setIsCookieDialogOpen] = useState(false);
+
   return (
-    <DropdownMenu className="min-w-64" anchor={anchor}>
-      <DropdownItem href="/account/settings">
-        <Cog6ToothIcon />
-        <DropdownLabel>Account Settings</DropdownLabel>
-      </DropdownItem>
-      <DropdownItem
-        href="https://rokbattles.com/legal/privacy-policy"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <ShieldCheckIcon />
-        <DropdownLabel>Privacy Policy</DropdownLabel>
-      </DropdownItem>
-      <DropdownDivider />
-      <DropdownItem onClick={() => handleLogout()}>
-        <ArrowRightStartOnRectangleIcon />
-        <DropdownLabel>Sign out</DropdownLabel>
-      </DropdownItem>
-    </DropdownMenu>
+    <>
+      <DropdownMenu className="min-w-64" anchor={anchor}>
+        <DropdownItem href="/account/settings">
+          <Cog6ToothIcon />
+          <DropdownLabel>Account Settings</DropdownLabel>
+        </DropdownItem>
+        <DropdownItem onClick={() => setIsCookieDialogOpen(true)}>
+          <ScaleIcon />
+          <DropdownLabel>Cookie Settings</DropdownLabel>
+        </DropdownItem>
+        <DropdownItem
+          href="https://rokbattles.com/legal/privacy-policy"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <ShieldCheckIcon />
+          <DropdownLabel>Privacy Policy</DropdownLabel>
+        </DropdownItem>
+        <DropdownDivider />
+        <DropdownItem onClick={() => handleLogout()}>
+          <ArrowRightStartOnRectangleIcon />
+          <DropdownLabel>Sign out</DropdownLabel>
+        </DropdownItem>
+      </DropdownMenu>
+      <CookieConsentDialog open={isCookieDialogOpen} onClose={() => setIsCookieDialogOpen(false)} />
+    </>
   );
 }
