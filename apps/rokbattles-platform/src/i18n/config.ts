a280a7a729e9eae5b51d0684a1a4d147e70c1f7f
nsylke <hello@nsylke.me>
2025-12-31 11:23:27 -0600

    feat: add es site translations

--- apps/rokbattles-platform/src/i18n/config.ts
+++ apps/rokbattles-platform/src/i18n/config.ts
@@ -2,8 +2,8 @@ export const languageCookieName = "platformLanguage";
 
 export const defaultLocale = "en";
 
-// temporarily disabling German (de), Spanish (es)
-export const supportedLocales = ["en", "ko"] as const;
+// temporarily disabling German (de)
+export const supportedLocales = ["en", "es", "ko"] as const;
 
 export type SupportedLocale = (typeof supportedLocales)[number];
 
@@ -13,7 +13,7 @@ export const languageOptions: ReadonlyArray<{
   flagCode: string;
 }> = [
   { locale: "en", label: "English", flagCode: "US" },
-  // { locale: "es", label: "Spanish", flagCode: "ES" },
+  { locale: "es", label: "Spanish", flagCode: "ES" },
   // { locale: "de", label: "German", flagCode: "DE" },
   { locale: "ko", label: "Korean", flagCode: "KR" },
 ];
