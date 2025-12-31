a8503600d395f8a59adb5800dadfbe7aeb0b075e
Nicholas Sylke <hello@nsylke.me>
2025-12-30 19:09:24 -0600

    chore: update sitemap on platform (#210)

--- apps/rokbattles-platform/src/app/sitemap.ts
+++ apps/rokbattles-platform/src/app/sitemap.ts
@@ -4,7 +4,7 @@ const BASE_URL = "https://platform.rokbattles.com";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const lastModified = new Date().toISOString().split("T")[0];
-  const routes = [""];
+  const routes = ["", "/olympian-arena", "/trends"];
 
   return routes.map((route) => ({
     url: `${BASE_URL}${route}`,
