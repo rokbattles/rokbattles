fd6a10a721ee616051461a7e80a475f6e5252bad
nsylke <hello@nsylke.me>
2026-01-04 19:14:46 -0600

    feat: add users and user_sessions tables

--- apps/rokbattles-site/src/db/schema.ts
+++ apps/rokbattles-site/src/db/schema.ts
@@ -1,6 +1,22 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  email: varchar({ length: 255 }),
+  discordId: varchar({ length: 255 }).unique().notNull(),
+  email: varchar({ length: 255 }).unique().notNull(),
+  username: varchar({ length: 255 }).notNull(),
+  globalName: varchar({ length: 255 }),
+  avatar: text(),
+  createdAt: timestamp({ withTimezone: true }).defaultNow().notNull(),
+  updatedAt: timestamp({ withTimezone: true }).defaultNow().notNull(),
+});
+
+export const userSessionsTable = pgTable("user_sessions", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  userId: integer()
+    .notNull()
+    .references(() => usersTable.id, { onDelete: "cascade" }),
+  sessionId: varchar({ length: 255 }).unique().notNull(),
+  createdAt: timestamp({ withTimezone: true }).defaultNow().notNull(),
+  expiresAt: timestamp({ withTimezone: true }).notNull(),
 });
