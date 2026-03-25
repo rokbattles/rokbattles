4dda1024dafdc2dd6d0529f8e779d23b89711b70
nsylke <hello@nsylke.me>
2026-03-24 23:27:43 -0500

    feat: add & organize pages to docs

--- nxt/src/app/(docs)/docs/[[...slug]]/page.tsx
+++ nxt/src/app/(docs)/docs/[[...slug]]/page.tsx
@@ -1,3 +1,5 @@
+import { findSiblings } from "fumadocs-core/page-tree";
+import { Card, Cards } from "fumadocs-ui/components/card";
 import { DocsBody, DocsDescription, DocsPage, DocsTitle } from "fumadocs-ui/layouts/docs/page";
 import { createRelativeLink } from "fumadocs-ui/mdx";
 import type { Metadata } from "next";
@@ -20,10 +22,34 @@ export default async function Page(props: PageProps<"/docs/[[...slug]]">) {
         <MDX
           components={getMDXComponents({
             a: createRelativeLink(source, page),
+            DocsCategory: ({ url }) => {
+              return <DocsCategory url={url ?? page.url} />;
+            },
           })}
         />
+        {page.data.index ? <DocsCategory url={page.url} /> : null}
       </DocsBody>
     </DocsPage>
+  );
+}
+
+function DocsCategory({ url }: { url: string }) {
+  return (
+    <Cards>
+      {findSiblings(source.getPageTree(), url).map((item) => {
+        if (item.type === "separator") return null;
+        if (item.type === "folder") {
+          if (!item.index) return null;
+          item = item.index;
+        }
+
+        return (
+          <Card key={item.url} title={item.name} href={item.url}>
+            {item.description}
+          </Card>
+        );
+      })}
+    </Cards>
   );
 }
 
