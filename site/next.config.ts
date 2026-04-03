e2b2f99180f939629042703c8ba00dbcbb7c2793
nsylke <hello@nsylke.me>
2026-04-03 15:58:13 -0500

    refactor: replace next-themes with wrksz/themes

--- site/next.config.ts
+++ site/next.config.ts
@@ -53,18 +53,6 @@ const config: NextConfig = {
         destination: "https://github.com/rokbattles/rokbattles/releases",
         permanent: false,
       },
-      {
-        // not implemented yet
-        source: "/register",
-        destination: "/",
-        permanent: false,
-      },
-      {
-        // not implemented yet
-        source: "/login",
-        destination: "/",
-        permanent: false,
-      },
     ];
   },
   async rewrites() {
