5636ed574d6a8752e766db639e36e59ff137e400
nsylke <hello@nsylke.me>
2026-04-04 11:32:36 -0500

    feat: configure fumadocs on site

--- site/next.config.ts
+++ site/next.config.ts
@@ -1,3 +1,4 @@
+import { createMDX } from "fumadocs-mdx/next";
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
@@ -16,7 +17,9 @@ const withNextIntl = createNextIntlPlugin({
   },
 });
 
-const plugins = [withNextIntl];
+const withMDX = createMDX({});
+
+const plugins = [withNextIntl, withMDX];
 const isProdEnv = process.env.NODE_ENV === "production";
 
 const config: NextConfig = {
