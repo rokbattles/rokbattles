3baf331efa1708b41445b50457ff0fab95cbbbfc
nsylke <hello@nsylke.me>
2026-04-09 16:36:54 -0500

    feat: implement docs pages

---
+++ site/src/app/(docs)/layout.tsx
@@ -0,0 +1,12 @@
+import { RootProvider } from "fumadocs-ui/provider/next";
+import "./globals.css";
+
+export default function Layout({ children }: LayoutProps<"/">) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className="flex flex-col min-h-screen">
+        <RootProvider>{children}</RootProvider>
+      </body>
+    </html>
+  );
+}
