9e1d647f6d88c9a4666cfe5a77d0243326593a8d
Nicholas Sylke <hello@nsylke.me>
2026-05-25 03:50:54 -0500

    fix: window builds failing without pcap (#579)

--- apps/rokbattles-tauri/src-tauri/build.rs
+++ apps/rokbattles-tauri/src-tauri/build.rs
@@ -1,3 +1,9 @@
 fn main() {
+    let target = std::env::var("TARGET").unwrap_or_default();
+    if target.ends_with("windows-msvc") {
+        println!("cargo:rustc-link-arg=/DELAYLOAD:wpcap.dll");
+        println!("cargo:rustc-link-lib=dylib=delayimp");
+    }
+
     tauri_build::build()
 }
