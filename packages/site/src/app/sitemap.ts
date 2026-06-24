04bbe1ced1132e9103bddd294916abacff5516ef
Nicholas Sylke <hello@nsylke.me>
2026-06-24 18:49:54 -0500

    refactor: update sitemap to include loot explorer (#665)

--- packages/site/src/app/sitemap.ts
+++ packages/site/src/app/sitemap.ts
@@ -6,7 +6,14 @@ const BASE_URL = "https://rokbattles.com";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const lastModified = new Date().toISOString().split("T")[0];
-  const routes = ["", "/olympian-arena", "/legal"];
+  const routes = [
+    "",
+    "/olympian-arena",
+    "/loot-explorer/barbarians",
+    "/loot-explorer/barbarian-forts",
+    "/loot-explorer/baulurs",
+    "/legal",
+  ];
 
   getLegalDocuments().map((doc) => routes.push(`/legal/${doc.slug}`));
   source.getPages().map((page) => routes.push(page.url));
