c8b20d18b17b70f158c31193a77f2be02aa10d89
nsylke <hello@nsylke.me>
2026-08-26 15:59:30 -0400

    refactor: add baseui & remove vite default files

--- packages/site-vite/src/main.tsx
+++ packages/site-vite/src/main.tsx
@@ -1,16 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router";
+import "./assets/globals.css";
 import App from "./App.tsx";
 
 // biome-ignore lint/style/noNonNullAssertion: required for vite
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
