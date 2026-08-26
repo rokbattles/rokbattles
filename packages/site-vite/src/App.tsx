1ec9935355372039ed38e5e580bac91a687f22eb
nsylke <hello@nsylke.me>
2026-08-26 18:05:45 -0400

    feat: add nuqs

--- packages/site-vite/src/App.tsx
+++ packages/site-vite/src/App.tsx
@@ -1,3 +1,4 @@
+import { NuqsAdapter } from "nuqs/adapters/react-router/v8";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
@@ -5,10 +6,12 @@ import NotFound from "./pages/NotFound.tsx";
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route index element={<Index />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <NuqsAdapter>
+        <Routes>
+          <Route index element={<Index />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </NuqsAdapter>
     </BrowserRouter>
   );
 }
