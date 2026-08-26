a2191c9f9bdeea1bd2a91212f9a97b499b7e18d1
nsylke <hello@nsylke.me>
2026-08-26 17:13:42 -0400

    refactor: add 404 route

--- packages/site-vite/src/App.tsx
+++ packages/site-vite/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Index from "./pages/Index.tsx";
+import NotFound from "./pages/NotFound.tsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Index />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
