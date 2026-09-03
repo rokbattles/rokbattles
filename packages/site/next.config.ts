f6def42dc49a6c29d61ef02ee9e1238276c73a92
nsylke <hello@nsylke.me>
2026-09-03 00:25:36 -0400

    chore: update import to new path for sentry/nextjs

--- packages/site/next.config.ts
+++ packages/site/next.config.ts
@@ -1,4 +1,4 @@
-import { withSentryConfig } from "@sentry/nextjs";
+import { withSentryConfig } from "@sentry/nextjs/config";
 import { createMDX } from "fumadocs-mdx/next";
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
