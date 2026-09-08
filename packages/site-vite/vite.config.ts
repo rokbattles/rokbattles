fad95ca0042a18b517656276020b62ad8ad490ab
nsylke <hello@nsylke.me>
2026-09-08 00:22:36 -0400

    feat: add tauri configuration/deps for unified app

--- packages/site-vite/vite.config.ts
+++ packages/site-vite/vite.config.ts
@@ -2,10 +2,25 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+const tauri = process.env.TAURI_DEV_HOST;
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   css: {
     transformer: "lightningcss",
+  },
+  clearScreen: tauri ? false : undefined,
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: tauri || false,
+    hmr: tauri
+      ? {
+          protocol: "ws",
+          host: tauri,
+          port: 1421,
+        }
+      : undefined,
   },
 });
