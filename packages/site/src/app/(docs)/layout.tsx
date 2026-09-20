a09bdd0517f4468e259638a5e2e5394cb9774758
Nicholas Sylke <hello@nsylke.me>
2026-09-19 20:02:43 -0400

    feat(site): add Discord component embeds (#933)

--- packages/site/src/app/(docs)/layout.tsx
+++ packages/site/src/app/(docs)/layout.tsx
@@ -1,9 +1,13 @@
 import { RootProvider } from "fumadocs-ui/provider/next";
+import { DiscordEmbed } from "@/components/discord-embed";
 import "./globals.css";
 
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <DiscordEmbed />
+      </head>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
